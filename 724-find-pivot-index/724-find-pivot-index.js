b572a1625b911e8702f643246ca65da26967bbf0
Teklehaimanot <teklehaimanotbelete@gmail.com>
2022-08-29 10:02:35 +0300

    Time: 115 ms (58.26%), Space: 44.5 MB (55.43%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -3,16 +3,19 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    let left = 0;
-    for(let i = 0; i < nums.length; i++){
-        let sum = 0
-        for(let j = i+1; j< nums.length; j++){
-            sum = sum + nums[j]
-        }
-        if(left === sum){
-            return i
-        }else left = left + nums[i]
-    }
-    
-    return -1
+let sum = 0;
+   for(let i = 0; i< nums.length; i++){
+      sum = sum + nums[i] 
+   }
+   
+   let left = 0; right = sum
+   for(let i = 0; i< nums.length; i++){
+       right = right - nums[i]
+       if(right === left){
+           return i
+       }else{
+           left = left + nums[i]
+       }
+   }
+return -1;
 };
